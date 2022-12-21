af73f66baf6aceaba625ce3c852465c45760c825
Dominikwigh <dominik-00@hotmail.com>
2022-12-21 09:55:13 +0000

    add first question and startgame function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,42 @@
 const startButton = document.getElementById('start-btn')
-constQuestionContainerElement = document.getElementById('question-container')
+const questionContainerElement = document.getElementById('question-container')
+const questionElment = document.getElementById('question')
+const answerButtonsElemnt = document.getElementById('answer-buttons')
+let shuffledQuestions, currentQuestionIndex 
 
 startButton.addEventListener('click', startGame)
 
 function startGame(){
     console.log('started')
     startButton.classList.add('hide')
-    constQuestionContainerElement.classList.remove('hide')
+    shuffledQuestions = questions.sort(() => Math.random() - 0.5)
+    currentQuestionIndex = 0
+    questionContainerElement.classList.remove('hide')
+    setNextQuestion()
 
 }
 
 function setNextQuestion(){
+    showQuestion(shuffledQuestions[currentQuestionIndex])
+}
 
+function showQuestion(question){
+     questionElment.innerText = question.question 
 }
 
 function selectAnswer(){
 
 }
+
+const questions = [
+    {
+        question: "The Scudetto is the name given to the league title in which European country?",
+        answers: {
+            a:'Portugal',
+            b:'Spain',
+            c:'Italy',
+            d:'England'
+        },
+        correctanswer: 'c'
+    },
+]
