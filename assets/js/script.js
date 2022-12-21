2888615fe6b6af17f479dafb2d6a2d67699e556e
Dominikwigh <dominik-00@hotmail.com>
2022-12-21 11:15:30 +0000

    add function to question

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,10 @@
-const startButton = document.getElementById('start-btn')
-const questionContainerElement = document.getElementById('question-container')
-const questionElment = document.getElementById('question')
-const answerButtonsElemnt = document.getElementById('answer-buttons')
-let shuffledQuestions, currentQuestionIndex 
+const startButton = document.getElementById('start-btn');
+const questionContainerElement = document.getElementById('question-container');
+const questionElement = document.getElementById('question');
+const answerButtonsElement = document.getElementById('answer-buttons');
+let shuffledQuestions, currentQuestionIndex;
 
-startButton.addEventListener('click', startGame)
+startButton.addEventListener('click', startGame);
 
 function startGame(){
     console.log('started')
@@ -18,25 +18,35 @@ function startGame(){
 
 function setNextQuestion(){
     showQuestion(shuffledQuestions[currentQuestionIndex])
-}
+};
 
 function showQuestion(question){
-     questionElment.innerText = question.question 
+     questionElement.innerText = question.question;
+     question.answers.forEach(answer => {
+        const button = createElement('button')
+        button.innerText = answer.text 
+        button.classList.add('btn')
+        if (answer-correct) {
+            button.dataset.correct = answer.correct 
+        }
+        button.addEventListener('click', selectAnswer)
+        answerButtonsElement.appendChild(button)
+     })
 }
 
 function selectAnswer(){
 
 }
-
+//Questions 
 const questions = [
     {
         question: "The Scudetto is the name given to the league title in which European country?",
-        answers: {
-            a:'Portugal',
-            b:'Spain',
-            c:'Italy',
-            d:'England'
-        },
-        correctanswer: 'c'
-    },
+        answers: [
+            { text: 'Portugal', correct: false },
+            { text: 'Spain', correct: false },
+            { text: 'Italy', correct: true },
+            { text: 'England', correct: false }
+        ]
+        
+    }
 ]
